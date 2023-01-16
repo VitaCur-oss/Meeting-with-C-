eacf9c6bae4d89d7d09e39b7611de6af109be61e
v <antyan2010@rambler.ru>
2023-01-16 13:42:39 +0300

    Программа, реализующую метод, который формирует массив случайных целых чисел из 8 элементов и выводит их на экран.

---
+++ Seminar_4/Task_29/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+Clear();
+
+// Напишите программу, реализующую метод, который формирует 
+// массив случайных целых чисел из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19,6, 1, 33 -> [1, 2, 5, 7, 19,6, 1, 33 ]
+Write("[");
+PrintArray(GetArray(8));
+Write("]");
+
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]}, ");
+    }
+}
