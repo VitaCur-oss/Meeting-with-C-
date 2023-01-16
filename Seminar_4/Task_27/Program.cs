db3970dd2c36f00829d0297321b5efbbd83a00b4
v <antyan2010@rambler.ru>
2023-01-16 12:35:43 +0300

    Программа, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.

---
+++ Seminar_4/Task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+//  Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine($"Сумма цифр в числе {a} равна {GetSum(a)}.");
+
+int GetSum(int x)
+{
+    int sum = 0;
+    while (x > 0)
+    {
+        sum += x % 10;
+        x = x / 10;
+    }
+    return sum;
+}
