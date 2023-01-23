cc874996ede230dc05f9968fcb774160ad6438c2
v <antyan2010@rambler.ru>
2023-01-23 15:22:23 +0300

    Программа, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.

--- Seminar_6/Task_43/Program.cs
+++ Seminar_6/Task_43/Program.cs
@@ -11,7 +11,6 @@ string[] aabb = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 double[] intersectionPoint = IntersectionPoint(int.Parse(aabb[0]), int.Parse(aabb[1]), int.Parse(aabb[2]), int.Parse(aabb[3]));
 WriteLine($"b1 = {aabb[0]}, k1 = {aabb[1]}, b2 = {aabb[2]}, k2 = {aabb[3]}  -> ({String.Join("; ", intersectionPoint)})");
 
-
 double[] IntersectionPoint(double A1, double B1, double A2, double B2)
 {
     double[] c = new double[2];
@@ -19,6 +18,7 @@ double[] IntersectionPoint(double A1, double B1, double A2, double B2)
     c[1] = Math.Round(B1 * c[0] + A1, 2);
     return c;
 }
+
 
 
 
