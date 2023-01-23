3d5c2277dadd3405dbf58167171d36ea4bf3eaa1
v <antyan2010@rambler.ru>
2023-01-23 15:20:24 +0300

    Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.

---
+++ Seminar_6/Task_41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+
+Write("Введите числа через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int poscount = PositiveCount(array);
+WriteLine($"{String.Join(", ",array)}  -> {poscount}");
+
+int PositiveCount(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (int.Parse(arr[i]) > 0) count++;
+        count += 0;
+    }
+    return count;
+}
