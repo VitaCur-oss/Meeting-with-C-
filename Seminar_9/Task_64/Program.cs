015e14d1fa8935e0ec065f7ad6f72e06ecf61808
v <antyan2010@rambler.ru>
2023-01-31 11:34:51 +0300

    Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.

---
+++ Seminar_9/Task_64/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+// Задача 64: Напишите программу, которая реализует рекурсивный метод вывода всех
+//  натуральных числел в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Write("Введите число N: ");
+int n = int.Parse(ReadLine()!);
+string number = GetNumber(n);
+WriteLine($"N = {n} -> '{number}'");
+
+string GetNumber(int num)
+{
+    return (num == 1)? num.ToString(): $"{num}, {GetNumber(num - 1)}";
+}
+    
